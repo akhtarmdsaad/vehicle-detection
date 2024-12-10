23db5ec057e9c10102c6c11c9c3d6b3bdd4aeb9a
Md Saad Akhtar <akhtarmdsaad@gmail.com>
2024-12-10 06:32:33 +0530

    added commands

---
+++ commands.py
@@ -0,0 +1,21 @@
+commands="""
+python3 -m venv venv
+python3 -m pip install --upgrade pip
+source venv/bin/activate
+pip install -r requirements.txt
+"""
+
+def run_command(command):
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+    for line in process.stdout:
+        print(line.decode().strip())
+
+def run_commands(commands):
+    for command in commands.split('\n'):
+        run_command(command)
+
+if __name__ == '__main__':
+    run_commands(commands)
+    print("Completed setup")
+
+
